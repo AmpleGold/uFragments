e587af6fbd715c454a43b0380326f0dfed5c0519
thegostep <thegostep@gmail.com>
2020-09-05 13:15:48 +0000

    add coverage

--- buidler.config.ts
+++ buidler.config.ts
@@ -3,9 +3,10 @@ import { usePlugin, BuidlerConfig } from '@nomiclabs/buidler/config'
 usePlugin('@nomiclabs/buidler-ethers')
 usePlugin('@nomiclabs/buidler-waffle')
 usePlugin('@openzeppelin/buidler-upgrades')
+usePlugin('solidity-coverage')
 
-export default <BuidlerConfig>{
+export default {
   solc: {
     version: '0.4.24',
   },
-}
+} as BuidlerConfig
